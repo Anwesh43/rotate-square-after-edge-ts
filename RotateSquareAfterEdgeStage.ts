d1cf24916df455b89bde95aa0c0f6dfe4e8d8d40
Anwesh43 <anweshthecool0@gmail.com>
2018-11-04 14:49:15 +0530

    adding RSAENode class

--- RotateSquareAfterEdgeStage.ts
+++ RotateSquareAfterEdgeStage.ts
@@ -76,3 +76,67 @@ class Animator {
         }
     }
 }
+
+const divideScale = (scale : number, i : number, n : number) : number => {
+    return Math.min(1/n, Math.max(0, scale - i * (1 / n))) * n
+}
+class RSAENode {
+    prev : RSAENode
+    next : RSAENode
+    state : State = new State()
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new RSAENode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = w / (nodes + 1)
+        context.lineWidth = Math.min(w, h) / 60
+        context.lineCap = 'round'
+        context.strokeStyle = 'teal'
+        const a : number = gap / 3
+        const deg : number = 2 * Math.PI / edges
+        context.save()
+        context.translate(this.i * (gap + 1), h/2)
+        const sc1 : number = divideScale(this.state.scale, 0, 2)
+        const sc2 : number = divideScale(this.state.scale, 1, 2)
+        context.rotate(Math.PI * sc2)
+        for (var i = 0; i < edges; i++) {
+            const sc : number = divideScale(sc1, i, edges)
+            context.save()
+            context.rotate(deg * i)
+            context.translate(0, a / Math.tan(deg/2))
+            context.moveTo(-a * sc, 0)
+            context.lineTo(a * sc, 0)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RSAENode {
+        var curr : RSAENode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
